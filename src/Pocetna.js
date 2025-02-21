f7942ed98c75feb7160b87db780f4c89101dce14
trinstone <2021.09.16@online.devetagimnazija.edu.rs>
2025-02-21 13:29:02 +0100

    uradjen uvodni tekst za pocetnu i pokrenuta pocetna i dodat stiker za pocetnu

---
+++ src/Pocetna.js
@@ -0,0 +1,19 @@
+const Pocetna = () => {
+
+    return(
+        <div className="Pocetna">
+            <h1>Dubinsko pranje i čišćenje raznih površina</h1>
+            <section>
+                Dajte povrsinu, mi cemo je ocistiti. Preko tvrdih podnih površina
+                i prozora, do automobila, namestaja, tepiha, jastuka, stolica.... 
+                Profesionalno dubinsko pranje i na veliko(restorani, hoteli) i malo.
+                <ol>
+                    <li>Dolazak na lice mesta</li>
+                    <li>Popusti na održavanje</li>
+                    <li>Radimo u najkraćem roku</li>
+                </ol>
+            </section>
+        </div>
+    );
+}
+export default Pocetna;
