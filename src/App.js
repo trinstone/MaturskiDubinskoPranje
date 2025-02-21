4b69926ab813bc70aa34898031729c78ceaa1eba
trinstone <2021.09.16@online.devetagimnazija.edu.rs>
2025-02-21 13:02:11 +0100

    opsta pozadina i hover navbar
    dodata slika koja je pozadina na svakoj stranici i hover za linkove na navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar';
+import pozadina from './pozadina.png';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ backgroundImage: `url(${pozadina})`, backgroundSize: '100% auto', // Ensures the whole image fits inside the div
+      backgroundPosition: 'top center', // Centers the image within the div
+      backgroundRepeat: 'no-repeat', // Ensures the image doesn't repeat
+      height: '100vh', // The height of the div is 30% of the viewport height
+      margin: 0
+    }}>
           <NavBar />
       </div>
     </Router>
